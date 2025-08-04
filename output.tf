90c1368e072a8d566f9b99f1e25ef8c04b267a92
swanand <sw611983@gmail.com>
2025-08-04 13:52:54 +0530

    Add files via upload

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "eks_cluster_id" {
-  value = aws_eks_cluster.my_cluster.id
-}
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.my_cluster.endpoint
-}
-
-output "eks_cluster_ca_cert" {
-  value = aws_eks_cluster.my_cluster.certificate_authority[0].data
+output "service_url" {
+  value = kubernetes_service.my_app_service.status.0.load_balancer.0.ingress.0.hostname
 }
