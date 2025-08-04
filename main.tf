cb993e89ad8caac816f02d7e989ba747805ed5a4
swanand <sw611983@gmail.com>
2025-08-04 13:52:40 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,70 +1,52 @@
-resource "aws_eks_cluster" "my_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role.arn
-  vpc_config {
-    subnet_ids = var.subnet_ids
+provider "kubernetes" {
+  host                   = var.eks_cluster_endpoint
+  token                  = var.eks_cluster_auth_token
+  cluster_ca_certificate = base64decode(var.eks_cluster_ca_cert)
+}
+
+resource "kubernetes_deployment" "my_app" {
+  metadata {
+    name = "my-app"
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "my-app"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "my-app"
+        }
+      }
+      spec {
+        container {
+          image = var.image_url
+          name  = "my-app"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
   }
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role" "eks_nodes_role" {
-  name               = "eks-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action   = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
-  role       = aws_iam_role.eks_nodes_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
-  role       = aws_iam_role.eks_nodes_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_nodes_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_eks_node_group" "my_node_group" {
-  cluster_name      = aws_eks_cluster.my_cluster.name
-  node_group_name   = "example-node-group"
-  node_role_arn     = aws_iam_role.eks_nodes_role.arn
-  subnet_ids        = var.subnet_ids
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+resource "kubernetes_service" "my_app_service" {
+  metadata {
+    name = "my-app-service"
   }
-  instance_types  = ["t3a.medium"]
-  ami_type        = "AL2_x86_64"
+  spec {
+    selector = {
+      app = "my-app"
+    }
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
 }
