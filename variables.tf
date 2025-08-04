56c8915d704fe5432bf01b0a7677d4d67925804a
swanand <sw611983@gmail.com>
2025-08-04 13:53:09 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,2 +1,4 @@
-variable "cluster_name" { type = string }
-variable "subnet_ids" { type = list(string) }
+variable "image_url" { type = string }
+variable "eks_cluster_endpoint" { type = string }
+variable "eks_cluster_ca_cert" { type = string }
+variable "eks_cluster_auth_token" { type = string }
